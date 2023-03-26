d4ed54208453b8b161c3abf0db5b81b46d9db53d
Lukeli0425 <lukeli0425@gmail.com>
2023-03-26 03:25:44 -0500

    update dataset

--- generate_data.py
+++ generate_data.py
@@ -5,23 +5,17 @@ import numpy as np
 from tqdm import tqdm
 import scipy.io as scio
 import matplotlib.pyplot as plt
+from utils.simulations import reorder_binary_sensor_data, read_images, join_images, split_images, forward_2D, delay_and_sum
 
-
-def generate_sections(vessel_data_path, dataset_path, n_sec=5, min_std=0.11):
+def generate_sections(logger, vessel_data_path, sec_path, n_sec=5, min_std=0.11):
     
-    if not os.path.exists(dataset_path):
-        os.mkdir(dataset_path)
-    sec_path = os.path.join(dataset_path, 'sections')
-    if not os.path.exists(sec_path):
-        os.mkdir(sec_path)
-
+    # Create directories.
     if not os.path.exists(os.path.join(sec_path, 'vis')):
         os.mkdir(os.path.join(sec_path, 'vis'))
     
     
     idx_sec = 0
-    # for idx in tqdm(range(3890)):
-    for idx in range(3890):
+    for idx in tqdm(range(3890)):
         data = scio.loadmat(os.path.join(vessel_data_path, f'{idx+1}.mat'))
         data = np.array(data['skin'])
         for i_sec in range(n_sec):
@@ -32,22 +26,63 @@ def generate_sections(vessel_data_path, dataset_path, n_sec=5, min_std=0.11):
                 np.save(os.path.join(sec_path, f'sec_{idx_sec}'), sec)
                 idx_sec += 1
                 
+                # Visualization.
                 if idx_sec < 30:
                     plt.imshow(sec)
                     plt.savefig(os.path.join(sec_path, 'vis', f'sec_{idx_sec}.jpg'))
-                    print(f'{idx_sec}: {sec.std()}')
-
-    print(idx_sec)
     
 
 
-def generate_data(data_path, n_train=10000, load_info=True):
-    vessel_files = os.listdir(data_path)
+def generate_data(dataset_path, vessel_data_path, n_train=10000, load_info=True, sectioning=True,
+                  image_size=(2024, 2024), PML_size=12, 
+                  sos_background = 1500.0, R_ring=0.05,
+                  n_delays=8, delay_step=2e-7):
+    logger = logging.getLogger('DataGenerator')
     
-    for idx in tqdm(range(3890)):
-        data = scio.loadmat(os.path.join(data_path, f'{idx+1}.mat'))
-        # print(data)
-        # skin = np.array(['skin'])
+    if not os.path.exists(dataset_path):
+        os.mkdir(dataset_path)
+    sec_path = os.path.join(dataset_path, 'sections')
+    if not os.path.exists(sec_path):
+        os.mkdir(sec_path)
+        
+    if sectioning:
+        logger.info('Creating sections...')
+        generate_sections(logger=logger,
+                          vessel_data_path=vessel_data_path,
+                          sec_path=sec_path, 
+                          n_sec=6)
+    
+    # Simulation parameters.
+    d_delays  = np.linspace(-(n_delays/2-1), n_delays/2, n_delays) * delay_step
+   
+    sec_files = os.listdir(sec_path)
+    for idx in tqdm(range(len(sec_files)//4)):
+        # Join and pad ground truth images.
+        gt_imgs = read_images(sec_path, sec_files[idx:idx+4])
+        gt_joint = join_images(gt_imgs)
+        
+        # K-wave 2D forward simulation.
+        sensor_data = forward_2D(gt_joint)
+        
+        # Delay and Sum Reconstruction.
+        obs_joint = []
+        for d_delay in d_delays:
+            recon = delay_and_sum(R_ring, 
+                                    kgrid.dt, 
+                                    medium.sound_speed_ref, 
+                                    sensor_data,
+                                    kgrid.x_vec[pad_start:pad_end], 
+                                    kgrid.y_vec[pad_start:pad_end],
+                                    d_delay=d_delay)
+            obs_joint.append(recon)
+        
+        # Split observation images.
+        images_obs = split_images(obs_joint, img_size=(128, 128))
+
+        # Save ground truth and observation images.
+        
+        
+        break
 
 
 if __name__ == '__main__':
@@ -55,10 +90,13 @@ if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
     parser.add_argument('--n_train', type=int, default=10000)
+    parser.add_argument('--sectioning', action="store_true")
+    parser.add_argument('--n_delays', type=int, default=8)
     opt = parser.parse_args()
     
-    # generate_data(data_path='/Users/luke/Downloads/skinVessel/', n_train=opt.n_train)
+    generate_data(dataset_path='/mnt/WD6TB/tianaoli/dataset/skinVessel_PACT/', 
+                  vessel_data_path='/mnt/WD6TB/tianaoli/skinVessel/',
+                  n_train=opt.n_train, sectioning=opt.sectioning,
+                  n_delays=opt.n_delays)
     
-    generate_sections(vessel_data_path='/mnt/WD6TB/tianaoli/skinVessel/', 
-                      dataset_path='/mnt/WD6TB/tianaoli/dataset/SkinVessel_PACT/', 
-                      n_sec=6)
+    
--- utils/simulations.py
+++ utils/simulations.py
@@ -4,6 +4,80 @@ import numba
 
 
 
+def read_images(path, files):
+    return [np.load(os.path.join(path, file)) for file in files]
+
+def join_images(images, n_row=2, n_col=2):
+    
+    if len(images) != n_row * n_col:
+        raise ValueError('Number of images does not match the number of rows and columns.')
+
+    images_row = []
+    for i in range(n_row):
+        images_row.append(np.concatenate(images[n_row*i:n_row*i+n_col], axis=1))
+    image_joint = np.concatenate(images_row , axis=0)
+    
+    return image_joint
+
+def split_images(image, img_size=(128, 128)):
+    images = []
+    n_x, n_y = image.shape[0]//img_size[0], image.shape[1]//img_size[1]
+    for idx in range(n_x):
+        for idy in range(n_y):
+            images.append(image[idx*img_size[0]:(idx+1)*img_size[0], idy*img_size[1]:(idy+1)*img_size[1]])
+    return images
+    
+
+def reorder_binary_sensor_data(sensor_data, sensor, kgrid, PML_size):
+
+    x_sensor = kgrid.x[sensor.mask[PML_size:-PML_size,PML_size:-PML_size] == 1]
+    y_sensor = kgrid.y[sensor.mask[PML_size:-PML_size,PML_size:-PML_size] == 1]
+    
+    angle = np.arctan2(-x_sensor, -y_sensor)
+    angle[angle < 0] = 2 * np.pi + angle[angle < 0]
+    reorder_index = np.argsort(angle)
+    
+    return sensor_data[reorder_index]
+
+
+@numba.jit(nopython=True) 
+def delay_and_sum(R_ring, T_sample, V_sound, Sinogram, ImageX, ImageY, d_delay=0):
+    """Generate a 2D Delay And Sum reconstructed PACT image of ring transducer array. This function is accelerated by `numba.jit` on a GPU.
+
+    Args:
+        R_ring (`float`): The R_ring [m] of the ring transducer array.
+        T_sample (`float`): Sample time interval of the signals [s].
+        V_sound (`float`): The sound speed [m/s] used for Delay And Sum reconstruction.
+        Sinogram (`numpy.ndarray`): A 2D array and each column of it is the signal recievde by one transducer. The nummber of transducers should be the number of columns. The transducers should be evenly distributed on a circle in counterclockwise arrangement and the first column correspond to the transducer in the dirrection `2pi/N` in the first quartile. The first sample should be at time 0 when the photoacoustic effect happens.
+        ImageX (`numpy.ndarray`): The vector [m] defining the x coordinates of the grid points on which the reconstruction is done. The values in the vector should be unifromly-spaced in ascending order. The origin of the cartesian coordinate system is the center of the ring array.
+        ImageY (`numpy.ndarray`): The vector [m] defining the y coordinates of the grid points on which the reconstruction is done. The values in the vector should be unifromly-spaced in ascending order. The origin of the cartesian coordinate system is the center of the ring array.
+        d_delay (`float`): The delay distance [m] of the signals used in DAS. The default value is 0.
+
+    Returns:
+        `numpy.ndarray`: A 2D array of size `(len(ImageY), len(ImageX))`. Image(t, s) is the reconstructed photoacoustic amplitude at the grid point (ImageX(s), ImageY(t)).
+    """
+
+    N_transducer = Sinogram.shape[0]
+    Image = np.zeros((len(ImageX), len(ImageY)))
+    delta_angle = 2*np.pi / N_transducer
+    angle_transducer = delta_angle * (np.arange(N_transducer,) + 1)
+
+    x_transducer = R_ring * np.sin(angle_transducer - np.pi)
+    y_transducer = R_ring * np.cos(angle_transducer - np.pi)
+    
+    related_data = np.zeros((N_transducer,))
+    
+    for s in range(len(ImageX)):
+        for t in range(len(ImageY)):
+            distance_to_transducer = np.sqrt((x_transducer - ImageX[s])**2 + (y_transducer - ImageY[t])**2) - d_delay
+            for k in range(N_transducer):
+                id = floor(distance_to_transducer[k]/(V_sound * T_sample))
+                if id > Sinogram.shape[1] or id < 0:
+                    related_data[k] = 0
+                else:
+                    related_data[k] = Sinogram[k, id]
+            Image[t, s] = related_data.mean()
+    return Image
 
 
 def subtract_background(image):
@@ -30,8 +104,10 @@ def zero_pad(image, pad_x, pad_y):
         `numpy.ndarray`: Padded image.
     """
     
-    image_pad = np.zeros((image.shape[0] + 2*pad_x, image.shape[1] + 2*pad_y))
-    image_pad[pad_x:-pad_x, pad_y:-pad_y] = image
+    image_pad = np.zeros((pad_x, pad_y))
+    
+    pad_start, pad_end = int(pad_x/2-image.shape[0]/2), int(pad_x/2+image.shape[0]/2)
+    image_pad[pad_start:pad_end, pad_start:pad_end] = image
     
     return image_pad
     
