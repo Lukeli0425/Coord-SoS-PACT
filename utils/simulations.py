f4c252d00b2c91170a6bb65f408702499b3f427c
Lukeli0425 <lukeli0425@gmail.com>
2024-01-16 18:46:29 -0600

    fixed ring error

--- utils/simulations.py
+++ utils/simulations.py
@@ -250,26 +250,29 @@ def forward_2D(p0, kgrid, medium, sensor, T_sample, PML_size=8):
 
 
 @numba.jit(nopython=True) 
-def delay_and_sum(R_ring, T_sample, V_sound, Sinogram, ImageX, ImageY, d_delay=0):
-    """Generate a 2D Delay And Sum reconstructed PACT image of ring transducer array. This function is accelerated by `numba.jit` on a GPU.
+def delay_and_sum(R_ring, T_sample, V_sound, Sinogram, ImageX, ImageY, d_delay=0, ring_error=0):
+    """Generate a 2D Delay And Sum recontructed PACT image of ring transducer array. This function is accelerated by `numba.jit` on a GPU.
 
     Args:
         R_ring (`float`): The R_ring [m] of the ring transducer array.
-        T_sample (`float`): Sample time interval of the signals [s].
-        V_sound (`float`): The sound speed [m/s] used for Delay And Sum reconstruction.
+        T_sample (`float`): Sample time interval [s] of the signals.
+        V_sound (`float`): The sound speed [m/s] used for Delay And Sum recontruction.
         Sinogram (`numpy.ndarray`): A 2D array and each column of it is the signal recievde by one transducer. The nummber of transducers should be the number of columns. The transducers should be evenly distributed on a circle in counterclockwise arrangement and the first column correspond to the transducer in the dirrection `2pi/N` in the first quartile. The first sample should be at time 0 when the photoacoustic effect happens.
-        ImageX (`numpy.ndarray`): The vector [m] defining the x coordinates of the grid points on which the reconstruction is done. The values in the vector should be unifromly-spaced in ascending order. The origin of the cartesian coordinate system is the center of the ring array.
-        ImageY (`numpy.ndarray`): The vector [m] defining the y coordinates of the grid points on which the reconstruction is done. The values in the vector should be unifromly-spaced in ascending order. The origin of the cartesian coordinate system is the center of the ring array.
+        ImageX (`numpy.ndarray`): The vector [m] defining the x coordinates of the grid points on which the recontruction is done. The values in the vector should be unifromly-spaced in ascending order. The origin of the cartesian coordinate system is the center of the ring array.
+        ImageY (`numpy.ndarray`): The vector [m] defining the y coordinates of the grid points on which the recontruction is done. The values in the vector should be unifromly-spaced in ascending order. The origin of the cartesian coordinate system is the center of the ring array.
         d_delay (`float`): The delay distance [m] of the signals used in DAS. The default value is 0.
+        ring_error (`numpy.ndarray`): The radial displacement error of the transducers. The default value is 0.
 
     Returns:
-        `numpy.ndarray`: A 2D array of size `(len(ImageY), len(ImageX))`. `Image[t, s]` is the reconstructed photoacoustic amplitude at the grid point `(ImageX[s], ImageY[t])`.
+        `numpy.ndarray`: A 2D array of size `(len(ImageY), len(ImageX))`. `Image[t, s]` is the recontructed photoacoustic amplitude at the grid point `(ImageX[s], ImageY[t])`.
     """
 
     N_transducer = Sinogram.shape[0]
     Image = np.zeros((len(ImageX), len(ImageY)))
     delta_angle = 2*np.pi / N_transducer
     angle_transducer = delta_angle * (np.arange(N_transducer,) + 1)
+    
+    R_ring = R_ring #- 40e-6 - 120e-6 * np.cos(2 * angle_transducer) + 5e-5 # Ring Array Correction 
 
     x_transducer = R_ring * np.sin(angle_transducer - np.pi)
     y_transducer = R_ring * np.cos(angle_transducer - np.pi)
@@ -278,7 +281,7 @@ def delay_and_sum(R_ring, T_sample, V_sound, Sinogram, ImageX, ImageY, d_delay=0
     
     for s in range(len(ImageX)):
         for t in range(len(ImageY)):
-            distance_to_transducer = np.sqrt((x_transducer - ImageX[s])**2 + (y_transducer - ImageY[t])**2) - d_delay
+            distance_to_transducer = np.sqrt((x_transducer - ImageX[s])**2 + (y_transducer - ImageY[t])**2) - d_delay + ring_error
             for k in range(N_transducer):
                 id = floor(distance_to_transducer[k]/(V_sound * T_sample))
                 if id > Sinogram.shape[1] or id < 0:
@@ -286,7 +289,6 @@ def delay_and_sum(R_ring, T_sample, V_sound, Sinogram, ImageX, ImageY, d_delay=0
                 else:
                     related_data[k] = Sinogram[k, id]
             Image[t, s] = related_data.mean()
-
     return Image
 
 
