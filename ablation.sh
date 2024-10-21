121539cbd1f872bb1576107b84ed09a182980a63
Lukeli0425 <lukeli0425@gmail.com>
2024-10-20 20:18:15 -0500

    update ablations

--- ablation.sh
+++ ablation.sh
@@ -39,10 +39,10 @@ gpu=1
 # done
 
 
-# # Learning rate.
-# for lr in {5e-3,2e-3,1e-3,5e-4,2e-4,1e-4}; do
-#   python reconstruction.py --task numerical --lam_tv 0e-5 --n_epochs 30 --lr $lr --gpu $gpu
-# done
+# Learning rate.
+for lr in {5e-3,2e-3,1e-3,5e-4,2e-4,1e-4}; do
+  python reconstruction.py --task numerical --sample_id 0 --lam_tv 1e-4 --n_epochs 30 --lr $lr --gpu $gpu
+done
 
 # for lr in {5e-3,2e-3,1e-3,5e-4,2e-4,1e-4}; do
 #   python reconstruction.py --task in_vivo --lam_tv 0e-5 --n_epochs 30 --lr $lr --gpu $gpu
@@ -74,4 +74,15 @@ gpu=1
 #   for n_delays in {64,32,16,8,4}; do
 #     python reconstruction.py --task numerical --sample_id $sample_id --method Deconv --n_delays $n_delays --gpu $gpu
 #   done
+# done
+
+
+# # Delay-and-sum.
+# for v_das in {1508,1509,1510,1511,1512}; do
+#   python reconstruction.py --task numerical --sample_id 0 --method DAS --v_das $v_das --gpu $gpu
+# done
+
+# # Dual-SOS DAS.
+# for v_body in {1557,1558,1559,1560,1561,1562,1563,1564}; do
+#   python reconstruction.py --task numerical --sample_id 0 --method Dual-SOS_DAS --v_body $v_body --gpu $gpu
 # done
