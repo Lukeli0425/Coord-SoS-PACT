3931c6cc8cc1069d2ef5a673f1537626588d98f0
Lukeli0425 <lukeli0425@gmail.com>
2025-02-26 15:56:23 -0600

    add apact IP

--- simulation.py
+++ simulation.py
@@ -110,7 +110,7 @@ if __name__ == "__main__":
     
     parser = argparse.ArgumentParser()
     parser.add_argument('--gpu', type=str, default='0', choices=['0', '1', '2'])
-    parser.add_argument('--sample_id', type=int, default=0, choices=[0, 1, 2, 3, 4])
+    parser.add_argument('--sample_id', type=int, default=0, choices=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
     args = parser.parse_args()
     
     # Select GPU.
