80ef9515bc9dba642ff095bd13fa58f834f8e49a
Lukeli0425 <lukeli0425@gmail.com>
2025-02-27 15:38:45 -0600

    update figures

--- ablation.sh
+++ ablation.sh
@@ -21,12 +21,12 @@ hls_df=0
 # done
 
 
-# Number of delays.
-for sample_id in {5,6,7,8,9}; do
-  for n_delays in {64,32,16,8,4,2,1}; do
-    python reconstruction.py --task numerical --sample_id $sample_id --n_delays $n_delays --hls $hls_df --hfs $hfs_df --n_epochs $n_epochs_df --lr $lr_df --batch_size 64 --gpu $gpu
-  done
-done
+# # Number of delays.
+# for sample_id in {5,6,7,8,9}; do
+#   for n_delays in {64,32,16,8,4,2,1}; do
+#     python reconstruction.py --task numerical --sample_id $sample_id --n_delays $n_delays --hls $hls_df --hfs $hfs_df --n_epochs $n_epochs_df --lr $lr_df --batch_size 64 --gpu $gpu
+#   done
+# done
 
 # for n_delays in {64,32,16,8,4,2,1}; do
 #   python reconstruction.py --task in_vivo --n_delays $n_delays --hls $hls_df --hfs $hfs_df --n_epochs $n_epochs_df --lr $lr_df --batch_size 64 --gpu $gpu
@@ -37,14 +37,14 @@ done
 # done
 
 
-# Network structure.
-for sample_id in {5,6,7,8,9}; do
-  for hfs in {16,32,64,128,256}; do
-    for hls in {0,1}; do
-      python reconstruction.py --task numerical --sample_id $sample_id --n_delays $n_delays_df --hfs $hfs --hls $hls --n_epochs $n_epochs_df --batch_size $bs_df --lr $lr_df --gpu $gpu
-    done
-  done
-done
+# # Network structure.
+# for sample_id in {5,6,7,8,9}; do
+#   for hfs in {16,32,64,128,256}; do
+#     for hls in {0,1}; do
+#       python reconstruction.py --task numerical --sample_id $sample_id --n_delays $n_delays_df --hfs $hfs --hls $hls --n_epochs $n_epochs_df --batch_size $bs_df --lr $lr_df --gpu $gpu
+#     done
+#   done
+# done
 
 # for hfs in {16,32,64,128,256}; do
 #   for hls in {0,1}; do
@@ -77,7 +77,7 @@ done
 
 ################### Pixel Grid ####################
 lr_pg_df=0.1
-lam_tv_df=1e-4
+lam_tv_df=0 # 1e-4
 # # Learning rate.
 # for lr in {2e-1,1e-1,5e-2,2e-2,1e-2,5e-3}; do
 #   python reconstruction.py --task numerical --sample_id 0 --n_delays $n_delays_df --method PG --lam_tv $lam_tv_df --n_epochs 30 --lr $lr --batch_size $bs_df --gpu $gpu
@@ -106,12 +106,12 @@ done
 #   python reconstruction.py --task in_vivo --n_delays $n_delays_df --method PG --n_delays $n_delays --lam_tv $lam_tv_df --n_epochs $n_epochs_df --lr $lr_pg_df --batch_size 64 --gpu $gpu
 # done
 
-# TV Regularization.
-for sample_id in {5,6,7,8,9}; do
-  for lam_tv in {1e-6,1e-5,1e-4,1e-3,1e-2}; do
-    python reconstruction.py --task numerical --sample_id $sample_id --n_delays $n_delays_df --method PG --lam_tv $lam_tv --n_epochs $n_epochs_df --lr $lr_pg_df --batch_size $bs_df --gpu $gpu
-  done
-done
+# # TV Regularization.
+# for sample_id in {5,6,7,8,9}; do
+#   for lam_tv in {0,1e-6,1e-5,1e-4,1e-3,1e-2}; do
+#     python reconstruction.py --task numerical --sample_id $sample_id --n_delays $n_delays_df --method PG --lam_tv $lam_tv --n_epochs $n_epochs_df --lr $lr_pg_df --batch_size $bs_df --gpu $gpu
+#   done
+# done
 
 # for lam_tv in {5e-6,1e-5,2e-5,5e-5,1e-4,2e-4,5e-4,1e-3}; do
 #   python reconstruction.py --task phantom --n_delays $n_delays_df --method PG --lam_tv $lam_tv --n_epochs $n_epochs_df --lr $lr_pg_df --batch_size $bs_df --gpu $gpu
@@ -143,12 +143,12 @@ done
 
 # ################### APACT ####################
 
-# Number of delays.
-for sample_id in {5,6,7,8,9}; do
-  for n_delays in {64,32,16,8,4,2,1}; do
-    python reconstruction.py --task numerical --sample_id $sample_id --method APACT --n_delays $n_delays --lam_tsv 5e-15 --n_iters 10 --lr 50 --gpu $gpu
-  done
-done
+# # Number of delays.
+# for sample_id in {5,6,7,8,9}; do
+#   for n_delays in {64,32,16,8,4,2,1}; do
+#     python reconstruction.py --task numerical --sample_id $sample_id --method APACT --n_delays $n_delays --lam_tsv 5e-15 --n_iters 10 --lr 50 --gpu $gpu
+#   done
+# done
 
 # for n_delays in {64,32,16,8,4,2,1}; do
 #   python reconstruction.py --task phantom --method APACT --n_delays $n_delays --lam_tsv 5e-15 --n_iters 10 --lr 50 --gpu $gpu
